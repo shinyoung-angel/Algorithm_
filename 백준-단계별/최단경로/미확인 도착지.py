b4a14bc529394bf5112987a455c146a3581c06fe
shin <sin5015243@naver.com>
2021-10-20 21:07:16 +0900

    10/20 DONE

--- 백준-단계별/최단경로/미확인 도착지.py
+++ 백준-단계별/최단경로/미확인 도착지.py
@@ -1,17 +1,59 @@
 
+import heapq
+import sys
+input = sys.stdin.readline
 
+def dijkstra(start):
+    global n
+
+    key = [123456789] * (n+1)
+    heap = []
+    heapq.heappush(heap, (0, start))
+    key[start] = 0
+
+    while heap:
+        w, node = heapq.heappop(heap)
+
+        # if key[n] < w: continue
+
+        for weight, idx in adj[node]:
+            next_weight = weight + w
+            if key[idx] > next_weight:
+                key[idx] = next_weight
+                heapq.heappush(heap, (next_weight, idx))
+    return key
 
 for tc in range(int(input())):
     n, m, t = map(int, input().split())
     s, g, h = map(int, input().split())
-    adj = [[0]*(n+1) for _ in range(n+1)]
+    adj = [[] for _ in range(n+1)]
 
     for i in range(m):
         st, ed, w = map(int, input().split())
-        adj[st][ed] = w
-        adj[ed][st] = w
+        adj[st].append((w, ed))
+        adj[ed].append((w, st))
 
-    destination = []
+    s_start = dijkstra(s)
+    g_start = dijkstra(g)
+    h_start = dijkstra(h)
+
+    path1 = s_start[g] + g_start[h]
+    path2 = s_start[h] + h_start[g]
+
+    ans_list = []
     for i in range(t):
-        destination.append(int(input()))
+        destination = int(input())
+        a = path1 + h_start[destination]
+        b = path2 + g_start[destination]
+        c = s_start[destination]
+        if a == c or b == c:
+            ans_list.append(destination)
+
+    ans_list.sort()
+
+    print(*ans_list)
+
+
+
+
 
