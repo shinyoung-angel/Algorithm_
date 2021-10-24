859ab7246dacff9db236d1811ab1f4319cc9706f
shin <sin5015243@naver.com>
2021-10-25 07:57:07 +0900

    10/25 DONE

--- 백준-단계별/백트래킹/N과M(2).py
+++ 백준-단계별/백트래킹/N과M(2).py
@@ -2,22 +2,21 @@ import sys
 input = sys.stdin.readline
 
 
-def perm(cnt):
+def perm(cnt, now_idx):
 
     if cnt == m:
         print(*sel)
         return
 
-    for i in range(1, n+1):
+    for i in range(now_idx, n+1):
         if not visited[i]:
             sel.append(i)
             visited[i] = 1
-            perm(cnt+1)
+            perm(cnt+1, i+1)            #### i+1을 호출!!!!!!!
             visited[i] = 0
             sel.pop()
 
 n, m = map(int, input().split())
 sel = []
 visited = [0] * (n+1)
-
-perm(0)
+perm(0, 1)
