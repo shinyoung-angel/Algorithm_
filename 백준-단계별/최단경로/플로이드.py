505f9dc9f47697ce37d1ea09fc3099f10350aac0
shin <sin5015243@naver.com>
2021-10-26 17:32:18 +0900

    10/26 DONE

--- 백준-단계별/최단경로/플로이드.py
+++ 백준-단계별/최단경로/플로이드.py
@@ -1,27 +1,36 @@
 import sys
-import heapq
 input = sys.stdin.readline
 
 
-
-def check(x):
-    key = [123456789] * (n+1)
-    key[x] = 0
-    heap = []
-    heapq.heappush(heap, (0, x))
-
-    while heap:
-        w, n = heapq.heappop(heap)
-
-        for weight, node in adj[n]:
-            next_weight = weight
-
 n = int(input())
 m = int(input())
-adj = [[] for _ in range(n+1)]
+adj = [[123456789]*(n+1) for _ in range(n+1)]
+
+
+## 이 과정이 없으면 안된다~~~~~~~~~~~~~~
 for i in range(m):
     a, b, c = map(int, input().split())
-    adj[a].append((c, b))
+    if adj[a][b] > c:
+        adj[a][b] = c
 
+## 자기 자신으로 가는 비용은 0으로 초기화
 for i in range(1, n+1):
-    check(i)
+    for j in range(1, n+1):
+        if i == j:
+            adj[i][j] = 0
+
+
+## 플로이드 워셜 알고리즘 수행
+for k in range(1, n+1):         ## 거쳐가는 정점
+    for a in range(1, n+1):     ## 시작 정점
+        for b in range(1, n+1): ## 도착 정점
+            adj[a][b] = min(adj[a][b], adj[a][k]+adj[k][b])
+
+## 연결되지 않은 도시들은 0으로 출력하기
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if adj[i][j] == 123456789:
+            print(0, end=' ')
+        else:
+            print(adj[i][j], end=' ')
+    print()
---
+++ 호곡/백준.py
@@ -0,0 +1,35 @@
+import sys
+
+input = sys.stdin.readline
+INF = int(1e9)
+
+n = int(input())
+m = int(input())
+
+graph = [[INF] * (n + 1) for _ in range(n + 1)]
+
+# for a in range(1, n + 1):
+#     for b in range(1, n + 1):
+#         if a == b:
+#             graph[a][b] = 0
+
+for _ in range(m):
+    s, e, c = map(int, input().split())
+    if graph[s][e] > c:
+        graph[s][e] = c
+
+for k in range(1, n + 1):
+    for a in range(1, n + 1):
+        for b in range(1, n + 1):
+            if a == b:
+                graph[a][b] = 0
+            else:
+                graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+
+for a in range(1, n + 1):
+    for b in range(1, n + 1):
+        if graph[a][b] == INF:
+            print(0, end=" ")
+        else:
+            print(graph[a][b], end=" ")
+    print()
