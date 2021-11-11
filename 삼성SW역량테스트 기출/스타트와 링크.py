b053f1390e0b1fa0d1e54bb9a4210ac88b70a91f
shin <sin5015243@naver.com>
2021-11-11 21:28:00 +0900

    11/11 DONE

---
+++ 삼성SW역량테스트 기출/스타트와 링크.py
@@ -0,0 +1,31 @@
+from itertools import combinations as comb
+import sys
+input = sys.stdin.readline
+
+
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+comb_list = list(comb(list(range(n)), n//2))        ## 두 조합으로 나누기
+comb_cnt = len(comb_list)
+result = 123456789
+
+for team in range(comb_cnt//2):                     ## 팀 하나를 뽑아 다시 조합 만들기
+    a_comb = list(comb(comb_list[team], 2))
+    b_comb = list(comb(comb_list[-team-1], 2))
+
+    a = b = 0
+    for persons in a_comb:
+        a += arr[persons[0]][persons[1]] + arr[persons[1]][persons[0]]
+
+    for persons in b_comb:
+        b += arr[persons[0]][persons[1]] + arr[persons[1]][persons[0]]
+
+    extract = abs(a-b)
+    result = min(result, extract)
+
+print(result)
+
+
+
+
