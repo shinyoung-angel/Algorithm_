f0b82a6f6b69ff9570d347a56475f0888d488cd4
shin <sin5015243@naver.com>
2021-12-20 23:10:55 +0900

    12/20/2021 DONE

--- 백준-단계별/dfs와bfs/로봇 청소기.py
+++ 백준-단계별/dfs와bfs/로봇 청소기.py
@@ -1,81 +1,43 @@
 
+## 북, 동, 하, 서 ( 시계방향 )
 dr = [-1, 0, 1, 0]
 dc = [0, 1, 0, -1]
 
 n, m = map(int, input().split())
 r, c, d = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
+## 방문 쳌
 visited = [[0]*m for _ in range(n)]
 
+## 시작지 방문쳌 and 카운트!
 visited[r][c] = 1
 cnt = 1
 
 while True:
-    flag = 0
-    for _ in range(4):
-        d = (d+3) % 4
+    flag = 0            ## 아직 아무것도 청소 안했음!
+    for _ in range(4):  ## 4방향을 돈다!
+        d = (d+3) % 4   ## 왼쪽방향으로 한 칸 돌린다! 중요!!!!!1
         nr = r + dr[d]
         nc = c + dc[d]
 
+        ## 범위 안에 들고, 빈 칸이고, 청소할 수 있다면!
+        ## 들려서 청소하고, 카운트하고, 현재 위치를 갱신하고, flag 변경!
         if 0 <= nr < n and 0 <= nc < m and arr[nr][nc] == 0:
             if visited[nr][nc] == 0:
                 visited[nr][nc] = 1
                 cnt += 1
                 r = nr
                 c = nc
-                flag = 1
+                flag = 1        ## 청소 했다는 뜻
                 break
 
-    if flag == 0:
+    if flag == 0:               ## 위의 for문에 들어가지 못했을 때
+        ## 즉 네 방향 모두 청소를 할 수 없을 때
+        ## 후진 했을 때 벽이면 break
+        ## 만약 뒤가 벽이 아니라면! 그 위치를 다시 갱신!!!
         if arr[r-dr[d]][c-dc[d]] == 1:
             print(cnt)
             break
         else:
             r, c = r-dr[d], c-dc[d]
 
-######################
-
-import sys
-input = sys.stdin.readline
-
-n,m = map(int,input().split())
-graph = []
-visited = [[0] * m for _ in range(n)]
-r,c,d = map(int,input().split())
-
-# d => 0,3,2,1 순서로 돌아야한다.
-dx = [-1,0,1,0]
-dy = [0,1,0,-1]
-
-for _ in range(n):
-    graph.append(list(map(int,input().split())))
-
-# 처음 시작하는 곳 방문 처리
-visited[r][c] = 1
-cnt = 1
-
-while 1:
-    flag = 0
-    # 4방향 확인
-    for _ in range(4):
-        # 0,3,2,1 순서 만들어주기위한 식
-        d = (d + 3) % 4
-        nx = r + dx[d]
-        ny = c + dy[d]
-        # 한번 돌았으면 그 방향으로 작업시작
-
-        if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 0:
-            if visited[nx][ny] == 0:
-                visited[nx][ny] = 1
-                cnt += 1
-                r = nx
-                c = ny
-                #청소 한 방향이라도 했으면 다음으로 넘어감
-                flag = 1
-                break
-    if flag == 0: # 4방향 모두 청소가 되어 있을 때,
-        if graph[r-dx[d]][c-dy[d]] == 1: #후진했는데 벽
-            print(cnt)
-            break
-        else:
-            r,c = r-dx[d],c-dy[d]
--- 삼성SW역량테스트 기출/컨베이어 벨트 위의 로봇.py
+++ 삼성SW역량테스트 기출/컨베이어 벨트 위의 로봇.py
@@ -1,28 +1,68 @@
+import sys
 
+input = sys.stdin.readline
 
-n, k = map(int, input().split())
-arr = list(map(int, input().split()))
-box = [0] * 2 * n
+N, K = map(int, input().split())
+belt = list(map(int, input().split()))
+robot = [False]*N
 
-box[0] = 1
-cnt = 0
-
+answer = 0
 while True:
+    answer += 1
+    belt.insert(0, belt.pop())
+    robot.insert(0, robot.pop())
+    robot[N-1] = False
 
-    for i in range(1, 2*n):
-        if box[i-1] == 1:
-            if box[i] == 0 and arr[i] >= 1:
-                box[i] = 1
-                box[i - 1] = 0
-                arr[i-1] -= 1
-            if arr[i-1] == 0:
-                cnt += 1
+    for i in range(N-2, -1, -1):
+        if robot[i] and robot[i+1] == False and belt[i+1]:
+            belt[i+1] -= 1
+            robot[i] = False
+            robot[i+1] = True
+    robot[N-1] = False
 
-    if box[0] == 0 and arr[0] >= 1:
-        box[0] = 1
+    if belt[0] and robot[0] == False:
+        belt[0] -= 1
+        robot[0] = True
 
-    if cnt == k:
+    count = belt.count(0)
+    if count >= K:
         break
 
-print(cnt)
+print(answer
+
+
+##############
+
+import sys
+from _collections import deque
+
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+belt = deque(map(int, input().split()))
+robot = deque([False]*N)
+
+answer = 0
+while True:
+    answer += 1
+    belt.rotate(1)
+    robot.rotate(1)
+    robot[N-1] = False
+
+    for i in range(N-2, -1, -1):
+        if robot[i] and robot[i+1] == False and belt[i+1]:
+            belt[i+1] -= 1
+            robot[i] = False
+            robot[i+1] = True
+    robot[N-1] = False
+
+    if belt[0] and robot[0] == False:
+        belt[0] -= 1
+        robot[0] = True
+
+    count = belt.count(0)
+    if count >= K:
+        break
+
+print(answer)
 
