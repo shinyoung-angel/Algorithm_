0a8e3f6fabd72334b549c6a0bc0550901e505fae
shin <sin5015243@naver.com>
2021-12-22 23:37:14 +0900

    12/22/2021 DONE

--- 호곡/SWEA.py
+++ 호곡/SWEA.py
@@ -1,2 +1,18 @@
 
 
+n, k = map(int, input().split())
+
+queue = list(range(1, n+1))
+tmp = 0
+result = []
+while queue:
+
+    for i in range(k-1):
+        queue.append(queue.pop(0))
+    result.append(queue[0])
+    queue.pop(0)
+
+
+result = str(result)
+ans = '<'+ result[1:-1] + '>'
+print(ans)
