5963c956fd7ce6fa30f4a7fafbbf9d020e33fa16
shin <sin5015243@naver.com>
2021-12-23 22:03:28 +0900

    12/23/2021 DONE

--- SWEA-4단계/틱택톰.py
+++ SWEA-4단계/틱택톰.py
@@ -1,46 +1,55 @@
 
 
-def get_winner(row):
-    x_cnt, o_cnt, t_cnt = 0, 0, 0
-    for c in row:
-        if c == 'X':
-            x_cnt += 1
-        elif c == 'O':
-            o_cnt += 1
-        elif c == 'T':
-            t_cnt += 1
-    if x_cnt + t_cnt == 4:
+### 행, 열, 대각선 총 10줄을 문자열로 변경하여 각 요소를 count
+
+## o, x, t를 카운트해주는 함수
+def check(word):
+    cnt_x, cnt_o, cnt_t = 0, 0, 0
+    for j in word:
+        if j == 'X': cnt_x += 1
+        elif j == 'O': cnt_o += 1
+        elif j == 'T': cnt_t += 1
+    if (cnt_t==1 and cnt_x==3) or cnt_x==4:
         return 'X won'
-    elif o_cnt + t_cnt == 4:
+    elif (cnt_t==1 and cnt_o==3) or cnt_o==4:
         return 'O won'
-    return None
+    return 'Draw'
 
 
-T = int(input())
-for t in range(1, T + 1):
-    if t != 1:
-        input()
-    board = []
-    done = True
-    for _ in range(4):
-        row = input()
-        if '.' in row:
-            done = False
-        board.append(row)
+for tc in range(1, int(input())+1):
+    ## 인풋 받을 때 주의!! 공백이 한 줄씩 있음
+    if tc == 1:
+        arr = [list(input().strip()) for _ in range(4)]
+    else:
+        arr = [list(input().strip()) for _ in range(5)]
+        arr.pop(0)
+
+    board = []                  ## 여기다가 문자열 담아줌
+    winner = ''
     for i in range(4):
-        vert = []
+        tmp_r = ''
+        tmp_c = ''
         for j in range(4):
-            vert.append(board[j][i])
-        board.append(''.join(vert))
-    board.append(''.join([board[0][0], board[1][1], board[2][2], board[3][3]]))
-    board.append(''.join([board[3][0], board[2][1], board[1][2], board[0][3]]))
-    for i in range(10):
-        winner = get_winner(board[i])
-        if winner:
-            print("#{} {}".format(t, winner))
+            tmp_r += arr[i][j]  ## 행
+            tmp_c += arr[j][i]  ## 열
+        board.append(tmp_r)
+        board.append(tmp_c)
+    board.append(''.join([arr[0][0], arr[1][1], arr[2][2], arr[3][3]])) # 왼쪽 대각선
+    board.append(''.join([arr[0][3], arr[1][2], arr[2][1], arr[3][0]])) # 오른쪽 대각선
+
+    for i in range(10):                 ## 총 10줄을 검사해줌
+        winner = check(board[i])
+        if winner != 'Draw':            ## 승자가 있으면 break
             break
-    if not winner:
-        if not done:
-            print("#{} Game has not completed".format(t))
-        else:
-            print("#{} Draw".format(t))
+
+    if winner == 'Draw':                ## 승자가 없을 때
+        flag = 0
+        for i in range(4):
+            for j in range(4):
+                if arr[i][j] == '.':    ## 게임이 끝나지 않았을 때
+                    winner = 'Game has not completed'
+                    flag = 1
+                    break
+            if flag == 1:
+                break
+    print('#{} {}'.format(tc, winner))
