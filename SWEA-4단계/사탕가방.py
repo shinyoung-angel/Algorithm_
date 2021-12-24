2146a10575b551269c89c4b6b723d3ce89d339ec
shin <sin5015243@naver.com>
2021-12-24 23:41:55 +0900

    12/24/2021 DONE

--- SWEA-4단계/사탕가방.py
+++ SWEA-4단계/사탕가방.py
@@ -1,33 +1,49 @@
 
+def Candy(N, M, nums):
+    # 가방의 최소개수
+    min_num = 1
+    # 가방의 최대개수(=사탕 종류 중 가장 많은 사탕의 개수)
+    max_num = max(nums)
 
-for tc in range(1, int(input())+1):
-    n, m = map(int, input().split())
-    candies = list(map(int, input().split()))
-    result = 0
+    # 이분탐색 답을 찾을 때까지
+    while min_num <= max_num:
+        # 최소 + 최대의 중간 지점 = 가방의 개수라고 가정
+        mid_num = (min_num + max_num) // 2
+        # 한 가방에 들어가는 사탕의 수
+        cnts = 0
 
-    if sum(candies) < m:
-        break
-    else:
+        for num in nums:
+            # 사탕의 종류별로, 한 가방에 넣을 수 있는 사탕의 수를 cnts에 더해줌
+            cnts += num // mid_num
 
+        # 가방에 들어가는 사탕의 수가 문제 조건과 같을 때
+        if cnts == M:
+            result_nums = []
+            for num in nums:
+                if num // mid_num != 0: # (사탕 개수 나누기 가방 개수 = 각 가방에 들어가는 사탕의 수)
+                    # 의 몫이 0이 아니면, 즉 담을 수 있는 사탕이면,
 
+                    # 사탕의 수를 각 가방에 들어가는 수로 나눈 값을 리스트에 담고
+                    result_nums.append(num // (num // mid_num))
+            # 가장 적은 값(모든 사탕이 들어갈 수 있는 최대의 가방 개수)을 반환
+            return min(result_nums)
 
-    print('#{} {}'.format(tc, result))
+            # 사탕 수가 모자라면(가정한 가방 수가 너무 많으면)
+        elif cnts < M:
+            # 최대 값을 하나 줄여 다시 이분 탐색
+            max_num = mid_num - 1
+        # 사탕 수가 너무 많으면(가정한 가방 수보다 많이 만들 수 있다면)
+        else:
+            # 최소 값을 하나 늘려 다시 이분 탐색
+            min_num = mid_num + 1
 
-#####################
+    return max_num
+
 
 T = int(input())
-for test_case in range(1, T + 1):
+
+for t in range(T):
     N, M = map(int, input().split())
-    A = list(map(int, input().split()))
-    low = 1
-    high = max(A)
-    while low <= high:
-        mid = (low+high)//2
-        total = 0
-        for e in A:
-            total += e//mid
-        if total < M:
-            high = mid - 1
-        else:
-            low = mid + 1
-    print("#", test_case, " ", high, sep='')
+    nums = [int(i) for i in input().split()]
+    result = Candy(N, M, nums)
+    print(f'#{t + 1} {result}')
