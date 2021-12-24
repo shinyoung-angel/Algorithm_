194571ec88cadfe0996760247870f2b91b0e883e
shin <sin5015243@naver.com>
2021-12-24 23:49:36 +0900

    12/24/2021 DONE

--- 호곡/백준.py
+++ 호곡/백준.py
@@ -1,2 +1,14 @@
+import sys
+input = sys.stdin.readline
 
+from collections import  deque
 
+n = int(input())
+
+arr = deque(list(range(1, n+1)))
+
+while len(arr) > 1:
+    arr.popleft()
+    arr.append(arr.popleft())
+
+print(*arr)
