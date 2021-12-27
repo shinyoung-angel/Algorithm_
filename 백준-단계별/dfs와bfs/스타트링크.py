a3526435a12db986275e6f87b95fe77780f0e026
shin <sin5015243@naver.com>
2021-12-27 10:13:48 +0900

    12/27/2021 DONE

--- 백준-단계별/dfs와bfs/스타트링크.py
+++ 백준-단계별/dfs와bfs/스타트링크.py
@@ -1,10 +1,45 @@
-
 import sys
 input = sys.stdin.readline
 from collections import deque
 
 ################ visited = [0] * (top+1)로 했을 때는 30%에서 오류가 났다
 ########근디 0 대신에 -1을 넣으니 통과네,,,,????
+
+top, start, company, u, d = map(int, input().split())
+dr = [u, -d]
+visited = [-1] * (top+1)
+
+def bfs(s):
+    queue = deque([s])
+    visited[s] = 0
+    while queue:
+        t = queue.popleft()
+
+        if t == company:
+            return
+        for i in range(2):
+            nt = t + dr[i]
+            if 1 <= nt <= top and visited[nt] == -1:
+                visited[nt] = visited[t] + 1
+                queue.append(nt)
+
+bfs(start)
+
+ans = visited[company]
+
+if ans == -1:
+    print('use the stairs')
+else:
+    print(ans)
+
+
+##############################
+
+
+
+
+### 이건 첫 방문지를 1로 해주고 출력 값에서 1을 빼주는  식으로 함
+#### 이건 또 왜 되노,,,,,,
 
 top, start, company, u, d = map(int, input().split())
 dr = [u, -d]
