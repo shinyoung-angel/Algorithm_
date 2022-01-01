21049a0b9d35a48a7156a7e1c2c7341312365246
shin <sin5015243@naver.com>
2022-01-01 23:04:19 +0900

    2022/01/01 Algorithm done

---
+++ 백준-단계별/정렬/좌표 정렬하기2.py
@@ -0,0 +1,8 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+arr.sort(key=lambda x:(x[1], x[0]))
+for i in arr:
+    print(*i)
