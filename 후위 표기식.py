bb74333238a330ae11d0920a11ec97f55409a51e
박신영 <sin5015243@naver.com>
2022-02-10 21:22:54 +0900

    2022/02/10 Algorithm done

--- 후위 표기식.py
+++ 후위 표기식.py
@@ -1,6 +1,8 @@
 import sys
 input = sys.stdin.readline
 
+## 후위 표기식 문제
+## 괄호와 연산자에 우선순위를 줌
 priority = {'(': 1, '+': 2, '-': 2, '*':3, '/': 3}
 
 test_case = input()
@@ -9,30 +11,30 @@ result = ''
 
 for i in test_case:
 
-    if i == '(': stack.append(i)
+    if i == '(': stack.append(i)    ## (는 바로 스택에 넣기
 
-    elif i.isalpha(): result += i
+    elif i.isalpha(): result += i   ## 알파벳은 result에 바로 넣기
 
     elif i in ['+', '-', '/', '*']:
-        if not stack or priority[stack[-1]] < priority[i]:
+        if not stack or priority[stack[-1]] < priority[i]: ## 스택이 비었거나 제일 마지막 연산자의 우선순위가 나보다 낮다면 스택에 바로 추가
             stack.append(i)
         else:
             while stack:
-                if priority[stack[-1]] >= priority[i]:
+                if priority[stack[-1]] >= priority[i]:      ## 그것이 아니라면 나의 우선순위가 젤 높을 때까지 pop해서 result에 추가
                     result += stack.pop()
                 else:
                      break
-            stack.append(i)
+            stack.append(i)             ## 이후 현재의 연산자를 스택에 넣기
 
-    elif i == ')':
+    elif i == ')':                      ## 닫느 괄호는 여는 괄호를 만날 때까지 스택을 pop하고 result에 추가 함
         while stack:
             if stack[-1] != '(':
                 result += stack.pop()
             else:
                 break
-        stack.pop()
+        stack.pop()                 ## 여는 괄호 pop
 
-while stack:
+while stack:                        ## 스택에 남아 있는 요소를 마저 result에 넣어줌
     result += stack.pop()
 
 print(result)
