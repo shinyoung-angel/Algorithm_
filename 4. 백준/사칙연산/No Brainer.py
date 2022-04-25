8fb0b747e173132e8f18f0e78450dad331bb479b
박신영 <sin5015243@naver.com>
2022-04-25 20:35:33 +0900

    2022/04/25 Algorithm done

---
+++ 4. 백준/사칙연산/No Brainer.py
@@ -0,0 +1,13 @@
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+for i in range(n):
+    a, b = map(int, input().split())
+
+    if a < b:
+        print('NO BRAINS')
+    else:
+        print('MMM BRAINS')
