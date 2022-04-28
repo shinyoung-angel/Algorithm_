2d4196a8f7212080d2005787bcf1ecd406995933
박신영 <sin5015243@naver.com>
2022-04-28 22:27:24 +0900

    2022/04/28 Algorithm done

---
+++ 4. 백준/사칙연산/Which Alien.py
@@ -0,0 +1,11 @@
+
+
+a = int(input())
+b = int(input())
+
+if a >= 3 and b <= 4:
+    print('TroyMartian')
+if a <= 6 and 2 <= b:
+    print('VladSaturnian')
+if a <=2 and b <= 3:
+    print('GraemeMercurian')
